f1b082dff72690f0c1b4666b74bbb1ae6bc6c10a
Stefan Stanić <stefan.stanic@yahoo.com>
2019-04-08 01:09:20 +0200

    SwaggerUI default page.

--- eKlinika.WebAPI/Startup.cs
+++ eKlinika.WebAPI/Startup.cs
@@ -34,7 +34,7 @@ namespace eKlinika.WebAPI
             
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+                c.SwaggerDoc("v1", new Info { Title = "eKlinika API", Version = "v1" });
             });
 
             var connection = @"Server=.;Database=eKlinika;Trusted_Connection=True;ConnectRetryCount=0";
@@ -59,7 +59,7 @@ namespace eKlinika.WebAPI
             // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "eKlinika API V1");
             });
 
             app.UseHttpsRedirection();
