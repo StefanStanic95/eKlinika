98461040fb639e7925a5fcd53f3f7a02ccb880e2
Emina Čustović <emina.custovic@edu.fit.ba>
2019-12-02 05:21:02 +0100

    Dodan upload profilne slike preko UWP.

--- eKlinika.MobileApp/eKlinika.MobileApp/Views/UrediProfilPage.xaml.cs
+++ eKlinika.MobileApp/eKlinika.MobileApp/Views/UrediProfilPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using eKlinika.MobileApp.ViewModels;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
 using System;
-
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -21,6 +23,110 @@ namespace eKlinika.MobileApp.Views
         {
             base.OnAppearing();
             await model.LoadKorisnika();
+        }
+
+        //Picture choose from device    
+        private async void btnSelectPic_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Error", "This is not support on your device.", "OK");
+                return;
+            }
+            else
+            {
+                var mediaOption = new PickMediaOptions()
+                {
+                    PhotoSize = PhotoSize.MaxWidthHeight
+                };
+                _mediaFile = await CrossMedia.Current.PickPhotoAsync();
+                if (_mediaFile == null) return;
+
+                Stream stream1 = _mediaFile.GetStream();
+                Stream stream2 = _mediaFile.GetStream();
+                byte[] resizedImage1 = null;
+                byte[] resizedImage2 = null;
+
+                resizedImage1 = ResizeImage(stream1);
+                resizedImage2 = ResizeImage(stream2);
+
+                Profilna.Source = ImageSource.FromStream(() => new MemoryStream(resizedImage1));
+                model.Korisnik.Slika= resizedImage2;
+            }
+        }
+
+        //Take picture from camera    
+        private async void btnTakePic_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":(No Camera available.)", "OK");
+                return;
+            }
+            else
+            {
+                _mediaFile = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                {
+                    Directory = "Sample",
+                    Name = "myImage.jpg",
+                    PhotoSize = PhotoSize.MaxWidthHeight
+                });
+
+                if (_mediaFile == null) return;
+
+                Stream stream1 = _mediaFile.GetStream();
+                Stream stream2 = _mediaFile.GetStream();
+                byte[] resizedImage1 = null;
+                byte[] resizedImage2 = null;
+
+                resizedImage1 = ResizeImage(stream1);
+                resizedImage2 = ResizeImage(stream2);
+
+                Profilna.Source = ImageSource.FromStream(() => new MemoryStream(resizedImage1));
+                model.Korisnik.Slika = resizedImage2;
+
+            }
+        }
+
+        private MediaFile _mediaFile;
+        private string URL { get; set; }
+
+
+        public void Busy()
+        {
+            uploadIndicator.IsVisible = true;
+            uploadIndicator.IsRunning = true;
+            btnSelectPic.IsEnabled = false;
+            btnTakePic.IsEnabled = false;
+            btnSave.IsEnabled = false;
+        }
+
+        public void NotBusy()
+        {
+            uploadIndicator.IsVisible = false;
+            uploadIndicator.IsRunning = false;
+            btnSelectPic.IsEnabled = true;
+            btnTakePic.IsEnabled = true;
+            btnSave.IsEnabled = true;
+        }
+
+
+        protected byte[] ResizeImage(Stream stream)
+        {
+            byte[] resizedImage = null;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                stream.CopyTo(ms);
+                resizedImage = ms.ToArray();
+            }
+
+            //if (Device.RuntimePlatform == Device.Android)
+            //    resizedImage = DependencyService.Get<IMediaService>().ResizeImage(resizedImage, 500, 500);
+
+            return resizedImage;
+
         }
     }
 }
